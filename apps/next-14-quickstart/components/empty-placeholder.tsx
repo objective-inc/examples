427e10884c6ff279b537d592a062823955e8c9b3
David Parks <Davidthomasparks@gmail.com>
2024-03-14 15:28:07 -0500

    Fix build errors

--- apps/next-14-quickstart/components/empty-placeholder.tsx
+++ apps/next-14-quickstart/components/empty-placeholder.tsx
@@ -6,77 +6,81 @@ import { Icons } from "@/components/icons"
 interface EmptyPlaceholderProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 export function EmptyPlaceholder({
-  className,
-  children,
-  ...props
+    className,
+    children,
+    ...props
 }: EmptyPlaceholderProps) {
-  return (
-    <div
-      className={cn(
-        "flex min-h-[400px] flex-col items-center justify-center  border border-dashed p-8 text-center animate-in fade-in-50",
-        className
-      )}
-      {...props}
-    >
-      <div className="mx-auto flex max-w-[420px] flex-col items-center justify-center text-center">
-        {children}
-      </div>
-    </div>
-  )
+    return (
+        <div
+            className={cn(
+                "flex min-h-[400px] flex-col items-center justify-center  border border-dashed p-8 text-center animate-in fade-in-50",
+                className
+            )}
+            {...props}
+        >
+            <div className="mx-auto flex max-w-[420px] flex-col items-center justify-center text-center">
+                {children}
+            </div>
+        </div>
+    )
 }
 
 interface EmptyPlaceholderIconProps
-  extends Partial<React.SVGProps<SVGSVGElement>> {
-  name: keyof typeof Icons
+    extends Partial<React.SVGProps<SVGSVGElement>> {
+    name: keyof typeof Icons
 }
 
 EmptyPlaceholder.Icon = function EmptyPlaceHolderIcon({
-  name,
-  className,
-  ...props
+    name,
+    className,
+    ...props
 }: EmptyPlaceholderIconProps) {
-  const Icon = Icons[name]
+    const Icon = Icons[name]
 
-  if (!Icon) {
-    return null
-  }
+    if (!Icon) {
+        return null
+    }
 
-  return (
-    <div className="flex h-20 w-20 items-center justify-center rounded-full bg-primary">
-      <Icon
-        className={cn("h-10 w-10 text-primary-foreground", className)}
-        {...props}
-      />
-    </div>
-  )
+    return (
+        <div className="flex h-20 w-20 items-center justify-center rounded-full bg-primary">
+            {/* @ts-ignore */}
+            <Icon
+                className={cn("h-10 w-10 text-primary-foreground", className)}
+                {...props}
+            />
+        </div>
+    )
 }
 
 interface EmptyPlacholderTitleProps
-  extends React.HTMLAttributes<HTMLHeadingElement> {}
+    extends React.HTMLAttributes<HTMLHeadingElement> {}
 
 EmptyPlaceholder.Title = function EmptyPlaceholderTitle({
-  className,
-  ...props
+    className,
+    ...props
 }: EmptyPlacholderTitleProps) {
-  return (
-    <h2 className={cn("mt-6 text-xl font-semibold", className)} {...props} />
-  )
+    return (
+        <h2
+            className={cn("mt-6 text-xl font-semibold", className)}
+            {...props}
+        />
+    )
 }
 
 interface EmptyPlacholderDescriptionProps
-  extends React.HTMLAttributes<HTMLParagraphElement> {}
+    extends React.HTMLAttributes<HTMLParagraphElement> {}
 
 EmptyPlaceholder.Description = function EmptyPlaceholderDescription({
-  className,
-  ...props
+    className,
+    ...props
 }: EmptyPlacholderDescriptionProps) {
-  return (
-    <p
-      className={cn(
-        "mb-8 mt-2 text-center text-sm font-normal leading-6 text-muted-foreground",
-        className
-      )}
-      {...props}
-    />
-  )
+    return (
+        <p
+            className={cn(
+                "mb-8 mt-2 text-center text-sm font-normal leading-6 text-muted-foreground",
+                className
+            )}
+            {...props}
+        />
+    )
 }
